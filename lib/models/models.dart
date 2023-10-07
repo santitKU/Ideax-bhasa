076f03f240d2f42c2f6c75a8c64bfded675e02d4
santitKU <ss26121321@student.ku,edu.np>
2023-10-08 03:28:41 +0545

    final final commit

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -10,8 +10,12 @@ class DataModel {
 }
 
 List<DataModel> dataList = [
-  DataModel("Indra Jatra", "assets/images/1.jpg", "Indra Jatra is a vibrant and culturally rich festival celebrated in the heart of Kathmandu, Nepal. It honors Lord Indra, the Hindu god of rain, with lively processions, traditional dances, and the raising of the iconic lingo (a ceremonial pole) amidst a lively atmosphere of devotion and festivity."),
-  DataModel("Bisket Jatra", "assets/images/2.jpg", "Bisket Jatra, also known as Nepalese New Year's Festival, is a lively and colorful celebration that marks the beginning of the Nepalese New Year in the town of Bhaktapur, Nepal. The festival includes the pulling of massive chariots, traditional music, dances, and various rituals that reflect the rich cultural heritage and vibrant spirit of the Newar community."),
-  DataModel("Rato Machhindranath Jatra", "assets/images/3.jpg", "ato Machhindranath Jatra is an annual chariot festival celebrated with great enthusiasm in the Kathmandu Valley of Nepal. It involves the procession of a massive wooden chariot carrying the deity Rato Machhindranath through the streets, bringing communities together in prayer and celebration, and is a prominent cultural event in the region."),
-  DataModel("Gai Jatra", "assets/images/4.jpg", "Gai Jatra is an amalgamation of mourning and celebration.In Bhaktapur and Lalitpur, it is customary to take a symbolic cow around the city, made of four bamboos. In Kathmandu, some children are even seen walking around, disguised as Krishna-Radha and Shiva-Parvati. Inspired, such a trend is seen even in Bhaktapur too."),
+  DataModel("इन्द्रजात्रा/Indra Jatra", "assets/images/1.jpg",
+      "इन्द्रजात्रा काठमाडौं, नेपालको मुटुमा मनाइने एक जीवन्त र सांस्कृतिक रूपमा धनी चाड हो। यसले भगवान इन्द्र, वर्षाका हिन्दू देवतालाई जीवन्त जुलुस, परम्परागत नृत्यहरू, र भक्ति र उत्सवको जीवन्त वातावरणको बीचमा प्रतिष्ठित लिङ्गो (एक औपचारिक पोल) उठाएर सम्मान गर्दछ।"),
+  DataModel("Bisket Jatra", "assets/images/2.jpg",
+      "Bisket Jatra, also known as Nepalese New Year's Festival, is a lively and colorful celebration that marks the beginning of the Nepalese New Year in the town of Bhaktapur, Nepal. The festival includes the pulling of massive chariots, traditional music, dances, and various rituals that reflect the rich cultural heritage and vibrant spirit of the Newar community."),
+  DataModel("Rato Machhindranath Jatra", "assets/images/3.jpg",
+      "ato Machhindranath Jatra is an annual chariot festival celebrated with great enthusiasm in the Kathmandu Valley of Nepal. It involves the procession of a massive wooden chariot carrying the deity Rato Machhindranath through the streets, bringing communities together in prayer and celebration, and is a prominent cultural event in the region."),
+  DataModel("Gai Jatra", "assets/images/4.jpg",
+      "Gai Jatra is an amalgamation of mourning and celebration.In Bhaktapur and Lalitpur, it is customary to take a symbolic cow around the city, made of four bamboos. In Kathmandu, some children are even seen walking around, disguised as Krishna-Radha and Shiva-Parvati. Inspired, such a trend is seen even in Bhaktapur too."),
 ];
